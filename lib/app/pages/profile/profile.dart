c3fc350f61cb29763b744aadab4f0047f080db54
Tiago Nascimento <tiago.nascimento@spruceid.com>
2021-03-08 11:37:13 -0300

    Add Future/Promise support to DIDKit interface

--- lib/app/pages/profile/profile.dart
+++ lib/app/pages/profile/profile.dart
@@ -8,7 +8,6 @@ import 'package:credible/app/shared/palette.dart';
 import 'package:credible/app/shared/widget/base/page.dart';
 import 'package:credible/app/shared/widget/info_dialog.dart';
 import 'package:credible/app/shared/widget/navigation_bar.dart';
-import 'package:credible/localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
