66526cff4c30a31ec25b5e05ed87d7c3b27dbbe7
Guilherme Souza <grsouza@pm.me>
2021-06-03 06:44:04 -0300

    Create model classes for holding document info

--- lib/app/pages/credentials/widget/document/body.dart
+++ lib/app/pages/credentials/widget/document/body.dart
@@ -1,15 +1,36 @@
 import 'package:credible/app/pages/credentials/models/credential.dart';
 import 'package:credible/app/pages/credentials/widget/document/item.dart';
+import 'package:credible/app/shared/ui/ui.dart';
+import 'package:credible/app/shared/widget/base/button.dart';
+import 'package:credible/app/shared/widget/tooltip_text.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class DocumentBodyWidgetModel {
+  final String issuedBy;
+  final String email;
+  final String npi;
+  final String issuedAt;
+
+  final Map<String, dynamic> rawData;
+
+  const DocumentBodyWidgetModel(
+      this.issuedBy, this.email, this.npi, this.issuedAt, this.rawData);
+
+  factory DocumentBodyWidgetModel.fromCredentialModel(CredentialModel model) =>
+      DocumentBodyWidgetModel(
+          model.issuer, 'email', 'npi', 'issuedAt', model.data);
+}
 
 class DocumentBody extends StatelessWidget {
-  final CredentialModel item;
+  final DocumentBodyWidgetModel model;
   final Widget? trailing;
 
   const DocumentBody({
     Key? key,
-    required this.item,
+    required this.model,
     this.trailing,
   }) : super(key: key);
 
@@ -25,28 +46,25 @@ class DocumentBody extends StatelessWidget {
                 Expanded(
                   child: DocumentItemWidget(
                     label: 'NPI:',
-                    value: '4876hTG97',
+                    value: model.npi,
                   ),
                 ),
                 const SizedBox(width: 8.0),
                 Expanded(
                   child: DocumentItemWidget(
                     label: 'Issued by:',
-                    value: item.issuer,
+                    value: model.issuedBy,
                   ),
                 ),
               ],
             ),
             const SizedBox(height: 20.0),
             DocumentItemWidget(
-              label: 'E-mail:',
-              value: 'richard@doximity.com',
+              label: 'Email:',
+              value: model.email,
             ),
             const SizedBox(height: 20.0),
-            DocumentItemWidget(
-              label: 'Issued at:',
-              value: 'San Francisco, CA',
-            ),
+            DocumentItemWidget(label: 'Issued at:', value: model.issuedAt),
             if (trailing != null) const SizedBox(height: 20.0),
             if (trailing != null) trailing!,
           ],
