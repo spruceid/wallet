1f1add539d4b5e89e059639abd22671d11a12273
Guilherme Souza <grsouza@pm.me>
2021-06-11 06:40:33 -0300

    Disable cloud backup of secure storage data

--- lib/app/interop/secure_storage/secure_storage_io.dart
+++ lib/app/interop/secure_storage/secure_storage_io.dart
@@ -1,5 +1,6 @@
 library secure_storage;
 
+import 'package:credible/app/shared/settings_storage.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'secure_storage.dart';
@@ -7,21 +8,32 @@ import 'secure_storage.dart';
 SecureStorageProvider getProvider() => SecureStorageIO();
 
 class SecureStorageIO extends SecureStorageProvider {
+  final _settings = SettingsStorage();
+
+  FlutterSecureStorage get _storage => FlutterSecureStorage();
+
+  Future<IOSOptions> get _defaultIOSOptions async {
+    final isCloudBackupEnabled = await _settings.isCloudBackupEnabled;
+    return IOSOptions(
+        groupId: 'com.spruceid.app.credible',
+        accessibility: isCloudBackupEnabled
+            ? IOSAccessibility.unlocked
+            : IOSAccessibility.unlocked_this_device);
+  }
+
   @override
   Future<String?> get(String key) async {
-    final storage = FlutterSecureStorage();
-    return storage.read(key: key);
+    return _storage.read(key: key, iOptions: await _defaultIOSOptions);
   }
 
   @override
   Future<void> set(String key, String val) async {
-    final storage = FlutterSecureStorage();
-    return storage.write(key: key, value: val);
+    return _storage.write(
+        key: key, value: val, iOptions: await _defaultIOSOptions);
   }
 
   @override
   Future<void> delete(String key) async {
-    final storage = FlutterSecureStorage();
-    return storage.delete(key: key);
+    return _storage.delete(key: key, iOptions: await _defaultIOSOptions);
   }
 }
