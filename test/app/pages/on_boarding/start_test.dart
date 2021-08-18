5aaba533022d70406bf4795b633450dd6ff29f11
Guilherme Souza <grsouza@pm.me>
2021-08-18 17:37:14 -0300

    Add AppLocalizations delegate to test file

--- test/app/pages/on_boarding/start_test.dart
+++ test/app/pages/on_boarding/start_test.dart
@@ -2,10 +2,12 @@ import 'package:credible/app/pages/on_boarding/start.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 MaterialApp _appWithHomeScreen(final WidgetTester tester) {
   return MaterialApp(
     localizationsDelegates: [
+      AppLocalizations.delegate,
       GlobalCupertinoLocalizations.delegate,
       GlobalMaterialLocalizations.delegate,
       GlobalWidgetsLocalizations.delegate,
