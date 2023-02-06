8404f015913ffac505178fcbcf1034b1983a8309
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-02-06 14:46:15 +0000

    Refactor DID display with FutureBuilder

--- lib/app/pages/did/display.dart
+++ lib/app/pages/did/display.dart
@@ -2,12 +2,10 @@ import 'package:credible/app/shared/ui/ui.dart';
 import 'package:credible/app/shared/widget/back_leading_button.dart';
 import 'package:credible/app/shared/widget/base/page.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 import 'package:credible/app/pages/did/models/did.dart';
 import 'package:credible/app/pages/did/widget/document.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 // TODO: implement the page for displaying a DID
@@ -26,32 +24,22 @@ class DIDDisplayPage extends StatefulWidget {
 }
 
 class _DIDDisplayPageState extends State<DIDDisplayPage> {
-  // TODO: determine how to initialize the didModel as it produces an uninitialized exception.
-  late DIDModel didModel;
+  // TODO: refactor into config
+  final base_endpoint = 'http://10.0.2.2:8081/did/';
 
-  // TODO: check how to pass the issuer DID of credential into function call
-  void get_did(String url) async {
-    final client = Dio();
-    final Map<String, dynamic> resolutionResult = jsonDecode((await client.get(url)).data);
-    setState(() {
-      didModel = DIDModel.fromMap(resolutionResult);
-    });
+  Future<DIDModel> get_did(String url) async {
+    return DIDModel.fromMap(jsonDecode((await http.get(Uri.parse(url))).body));
   }
 
   @override
   void initState() {
-    super.initState(); //TODO: Add placeholder DIDModel with empty details as this produces error until get_did() is called
-    final url = 'http://10.0.2.2:8081/did/${widget.name}';
-    get_did(url);
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final didDocModel = didModel;
-
     final localizations = AppLocalizations.of(context)!;
-    print(widget.name);
-    print(widget.data);
+    final url = '$base_endpoint${widget.name}';
     return BasePage(
       title: 'DID Resolution',
       titleLeading: BackLeadingButton(),
@@ -77,18 +65,33 @@ class _DIDDisplayPageState extends State<DIDDisplayPage> {
                   ),
                   Container(
                     alignment: Alignment.center,
-                    padding: const EdgeInsets.all(32.0),
-                    child: DIDDocumentWidget(
-                      model:
-                      DIDDocumentWidgetModel.fromDIDModel(didDocModel)
-                      ),
-                    
-                    // child: QrImage(
-                    //   data: data,
-                    //   version: QrVersions.auto,
-                    //   foregroundColor: UiKit.palette.icon,
-                    // ),
-                  ),
+                    padding: const EdgeInsets.all(8.0),
+                    // See here for FutureBuilder:
+                    // https://api.flutter.dev/flutter/widgets/FutureBuilder-class.html
+                    child: FutureBuilder<DIDModel>(
+                        future: get_did(url),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<DIDModel> snapshot) {
+                          // Uncomment for waiting part.
+                          // if (false) {
+                          if (snapshot.hasData) {
+                            return DIDDocumentWidget(
+                                model: DIDDocumentWidgetModel.fromDIDModel(
+                                    snapshot.data!));
+                          } else {
+                            return Center(
+                                child: Column(
+                              children: [
+                                SizedBox(
+                                  width: 100,
+                                  height: 100,
+                                  child: CircularProgressIndicator(),
+                                ),
+                              ],
+                            ));
+                          }
+                        }),
+                  )
                 ],
               ),
             ),
