ef56ddb4885c4a3a8c92c917bd4cbb51b736dbd2
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-02-10 15:24:08 +0000

    Update test data

--- test/app/pages/did/models/did_test.dart
+++ test/app/pages/did/models/did_test.dart
@@ -2,39 +2,36 @@ import 'package:credible/app/pages/did/models/did.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 const DID_EXAMPLE = {
-  // 'data': {
-    'didDocument': {
-      '@context': [
-        'https://www.w3.org/ns/did/v1',
-        {'@base': 'did:ion:test:EiAtHHKFJWAk5AsM3tgCut3OiBY4ekHTf66AAjoysXL65Q'}
-      ],
-      'id': 'did:ion:test:EiAtHHKFJWAk5AsM3tgCut3OiBY4ekHTf66AAjoysXL65Q',
-      'controller':
-          'did:ion:test:EiBVpjUxXeSRJpvj2TewlX9zNF3GKMCKWwGmKBZqF6pk_A',
-      'verificationMethod': [
-        {
-          'id': '#ePyXsaNza8buW6gNXaoGZ07LMTxgLC9K7cbaIjIizTI',
-          'type': 'JsonWebSignature2020',
-          'controller':
-              'did:ion:test:EiAtHHKFJWAk5AsM3tgCut3OiBY4ekHTf66AAjoysXL65Q',
-          'publicKeyJwk': {
-            'kty': 'EC',
-            'crv': 'secp256k1',
-            'x': '0nnR-pz2EZGfb7E1qfuHhnDR824HhBioxz4E-EBMnM4',
-            'y': 'rWqDVJ3h16RT1N-Us7H7xRxvbC0UlMMQQgxmXOXd4bY'
-          }
+  'didDocument': {
+    '@context': [
+      'https://www.w3.org/ns/did/v1',
+      {'@base': 'did:ion:test:EiAtHHKFJWAk5AsM3tgCut3OiBY4ekHTf66AAjoysXL65Q'}
+    ],
+    'id': 'did:ion:test:EiAtHHKFJWAk5AsM3tgCut3OiBY4ekHTf66AAjoysXL65Q',
+    'controller': 'did:ion:test:EiBVpjUxXeSRJpvj2TewlX9zNF3GKMCKWwGmKBZqF6pk_A',
+    'verificationMethod': [
+      {
+        'id': '#ePyXsaNza8buW6gNXaoGZ07LMTxgLC9K7cbaIjIizTI',
+        'type': 'JsonWebSignature2020',
+        'controller':
+            'did:ion:test:EiAtHHKFJWAk5AsM3tgCut3OiBY4ekHTf66AAjoysXL65Q',
+        'publicKeyJwk': {
+          'kty': 'EC',
+          'crv': 'secp256k1',
+          'x': '0nnR-pz2EZGfb7E1qfuHhnDR824HhBioxz4E-EBMnM4',
+          'y': 'rWqDVJ3h16RT1N-Us7H7xRxvbC0UlMMQQgxmXOXd4bY'
         }
-      ],
-      'service': [
-        {
-          'id': '#TrustchainID',
-          'type': 'Identity',
-          'serviceEndpoint':
-              'https://identity.foundation/ion/trustchain-root-plus-2'
-        }
-      ]
-    }
-  // }
+      }
+    ],
+    'service': [
+      {
+        'id': '#TrustchainID',
+        'type': 'Identity',
+        'serviceEndpoint':
+            'https://identity.foundation/ion/trustchain-root-plus-2'
+      }
+    ]
+  }
 };
 
 void main() {
