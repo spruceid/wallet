20a0d251e2e6d30982374d09354b46d667c5adab
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-03-06 15:40:18 +0000

    Remove unnecessary assignment

--- lib/app/pages/did/models/chain.dart
+++ lib/app/pages/did/models/chain.dart
@@ -8,11 +8,9 @@ class DIDChainModel {
   // Assumed map is an HTTP API resolved DIDChain
   factory DIDChainModel.fromMap(Map<String, dynamic> data) {
     assert(data.containsKey('didChain'));
-    var mapped =
-        data['didChain']!.map<DIDModel>((m) => DIDModel.fromMap(m)).toList();
-
     return DIDChainModel(
-      didChain: mapped,
+      didChain:
+          data['didChain']!.map<DIDModel>((m) => DIDModel.fromMap(m)).toList(),
     );
   }
 
