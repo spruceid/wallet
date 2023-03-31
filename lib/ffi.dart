1bc56446ca027033c46d27ec82c472e5722b362c
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-03-31 20:37:45 +0100

    Update build and FFI crate

--- lib/ffi.dart
+++ lib/ffi.dart
@@ -11,7 +11,7 @@ import 'bridge_generated.dart';
 export 'bridge_generated.dart';
 import 'dart:io' as io;
 
-const _base = 'trustchain_ion';
+const _base = 'trustchain_ffi';
 
 // On MacOS, the dynamic library is not bundled with the binary,
 // but rather directly **linked** against the binary.
