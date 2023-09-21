03eaed6d5cd1168e8811d4bb2771ca773e983a64
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-09-21 14:20:05 +0100

    Refactor as cannot be null, add comment for remaining todo

--- lib/app/shared/config.dart
+++ lib/app/shared/config.dart
@@ -5,35 +5,27 @@ import 'package:credible/app/shared/constants.dart';
 
 class FFIConfig {
   Future<int> get_root_event_time() async {
-    final rootEventTime = await SecureStorageProvider.instance
-            .get(ConfigModel.rootEventTimeKey) ??
-        Constants.ffiConfig['trustchainOptions']!['rootEventTime']!;
+    final rootEventTime = (await SecureStorageProvider.instance
+        .get(ConfigModel.rootEventTimeKey))!;
     return int.parse(rootEventTime);
   }
 
   Future<String> get_trustchain_endpoint() async {
-    return await SecureStorageProvider.instance
-            .get(ConfigModel.trustchainEndpointKey) ??
-        '10.0.2.2:8081';
+    return (await SecureStorageProvider.instance
+        .get(ConfigModel.trustchainEndpointKey))!;
   }
 
   Future<String> get_ion_endpoint() async {
-    return await SecureStorageProvider.instance
-            .get(ConfigModel.ionEndpointKey) ??
-        '10.0.2.2:3000';
+    return (await SecureStorageProvider.instance
+        .get(ConfigModel.ionEndpointKey))!;
   }
 
   Future<String> get_did() async {
-    final key = (await SecureStorageProvider.instance.get('key'))!;
-    final did_key =
-        DIDKitProvider.instance.keyToDID(Constants.defaultDIDMethod, key);
-    final did_stored =
-        (await SecureStorageProvider.instance.get(ConfigModel.didKey))!;
-    final did = did_stored == '' ? did_key : did_stored;
-    return did;
+    return (await SecureStorageProvider.instance.get(ConfigModel.didKey))!;
   }
 
   Future<Map<String, Map<dynamic, dynamic>>> get_ffi_config() async {
+    // TODO [#40]: revisit after refactor to use https
     var ffiConfig = Constants.ffiConfig;
     ffiConfig['trustchainOptions']!['rootEventTime'] =
         await get_root_event_time();
