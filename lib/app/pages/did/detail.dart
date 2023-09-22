155b24fc11e858a3ad2ec48cee3846314e8283f9
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-09-22 10:26:30 +0100

    Add comment, remove unused imports

--- lib/app/pages/did/detail.dart
+++ lib/app/pages/did/detail.dart
@@ -1,23 +1,14 @@
 // import 'package:credible/app/shared/ui/ui.dart';
-import 'package:credible/app/shared/config.dart';
-import 'package:credible/app/shared/constants.dart';
 import 'package:credible/app/shared/globals.dart';
 import 'package:credible/app/shared/widget/back_leading_button.dart';
 import 'package:credible/app/shared/widget/base/page.dart';
 import 'package:flutter/material.dart';
 import 'package:credible/app/pages/did/models/did.dart';
-import 'package:credible/app/pages/did/widget/document.dart';
 import 'package:flutter_json_viewer/flutter_json_viewer.dart';
 // No localizations currently on this page
 // import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:flutter_modular/flutter_modular.dart';
 
-// Trustchain FFI
-import 'package:credible/app/interop/trustchain/trustchain.dart';
-
-// TODO: implement the page for displaying a DID
 class DIDDisplayPage extends StatefulWidget {
   final String did;
 
@@ -31,16 +22,6 @@ class DIDDisplayPage extends StatefulWidget {
 }
 
 class _DIDDisplayPageState extends State<DIDDisplayPage> {
-  // MOVED TO shared/globals.dart:
-  // Future<DIDModel> get_did(String did) async {
-  //   final url =
-  //       (await ffi_config_instance.get_trustchain_endpoint()) + '/did/' + did;
-  //   final url_split = url.split('/');
-  //   final route = '/' + url_split.sublist(1).join('/');
-  //   final uri = Uri.http(url_split[0], route);
-  //   return DIDModel.fromMap(jsonDecode((await http.get(uri)).body));
-  // }
-
   @override
   void initState() {
     super.initState();
@@ -66,14 +47,9 @@ class _DIDDisplayPageState extends State<DIDDisplayPage> {
                   Container(
                     alignment: Alignment.center,
                     padding: const EdgeInsets.all(8.0),
-                    // See here for FutureBuilder:
-                    // https://api.flutter.dev/flutter/widgets/FutureBuilder-class.html
-                    // child: FutureBuilder<DIDModel>(
-
                     child: FutureBuilder<DIDModel>(
                         future: resolve_did(widget.did),
                         builder: (BuildContext context,
-                            // AsyncSnapshot<DIDModel> snapshot) {
                             AsyncSnapshot<DIDModel> snapshot) {
                           if (snapshot.hasData) {
                             return GestureDetector(
@@ -85,6 +61,8 @@ class _DIDDisplayPageState extends State<DIDDisplayPage> {
                                     );
                                   }
                                 },
+                                // TODO [#22]: consider reverting to the same widget
+                                // as used in DID Chain (i.e. expandable JSON Viewer)
                                 // child: DIDDocumentWidget(
                                 //     model: DIDDocumentWidgetModel.fromDIDModel(
                                 //         snapshot.data!)));
